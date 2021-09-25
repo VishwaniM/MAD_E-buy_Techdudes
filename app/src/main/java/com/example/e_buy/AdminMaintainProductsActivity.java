5e1bbcaed8e3917f7fc74a2a6466e65d52ae55eb
Sameera <Sameeranicho@gmail.com>
2021-09-25 20:57:03 +0530

    MaintainProduct_EditProducts_DeleteFunctionAdded

--- app/src/main/java/com/example/e_buy/AdminMaintainProductsActivity.java
+++ app/src/main/java/com/example/e_buy/AdminMaintainProductsActivity.java
@@ -24,7 +24,7 @@ import java.util.HashMap;
 
 public class AdminMaintainProductsActivity extends AppCompatActivity {
 
-    private Button applyChangesBtn;
+    private Button applyChangesBtn,deleteBtn;
     private EditText name, price, description;
     private ImageView imageView;
     private String productID = "";
@@ -44,6 +44,7 @@ public class AdminMaintainProductsActivity extends AppCompatActivity {
         price = findViewById(R.id.product_price_maintain);
         description = findViewById(R.id.product_description_maintain);
         imageView = findViewById(R.id.product_image_maintain);
+        deleteBtn = findViewById(R.id.delete_product_btn);
 
 
         displaySpecificProductInfo();
@@ -54,6 +55,28 @@ public class AdminMaintainProductsActivity extends AppCompatActivity {
                 applyChanges();
             }
         });
+
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteThisProduct();
+            }
+        });
+
+    }
+
+    private void deleteThisProduct() {
+
+        productsRef.removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Intent intent = new Intent(AdminMaintainProductsActivity.this,AdminCategoryActivity.class);
+                startActivity(intent);
+                finish();
+                Toast.makeText(AdminMaintainProductsActivity.this,"The Product is deleted Successfully",Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 
