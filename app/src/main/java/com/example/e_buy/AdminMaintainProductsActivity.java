5b68a34db7f0929f6158ff75d1679f1a45417a61
Sameera <Sameeranicho@gmail.com>
2021-09-25 20:18:46 +0530

    MaintainProduct_EditProductsDone

--- app/src/main/java/com/example/e_buy/AdminMaintainProductsActivity.java
+++ app/src/main/java/com/example/e_buy/AdminMaintainProductsActivity.java
@@ -1,27 +1,130 @@
 package com.example.e_buy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
 
 public class AdminMaintainProductsActivity extends AppCompatActivity {
 
     private Button applyChangesBtn;
     private EditText name, price, description;
     private ImageView imageView;
+    private String productID = "";
+    private DatabaseReference productsRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_maintain_products);
 
+        productID = getIntent().getStringExtra("pid");
+        productsRef = FirebaseDatabase.getInstance().getReference().child("Products").child(productID);
+
+
         applyChangesBtn = findViewById(R.id.apply_changes_btn);
         name = findViewById(R.id.product_name_maintain);
         price = findViewById(R.id.product_price_maintain);
         description = findViewById(R.id.product_description_maintain);
         imageView = findViewById(R.id.product_image_maintain);
+
+
+        displaySpecificProductInfo();
+
+        applyChangesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                applyChanges();
+            }
+        });
+
+    }
+
+    private void applyChanges() {
+        String pName = name.getText().toString();
+        String pPrice = price.getText().toString();
+        String pDescription = description.getText().toString();
+
+        if(pName.equals("")){
+            Toast.makeText(this,"Write Down Product Name",Toast.LENGTH_SHORT).show();
+        }
+        else if(pPrice.equals("")){
+            Toast.makeText(this,"Write Down Product Price",Toast.LENGTH_SHORT).show();
+        }
+        else if(pDescription.equals("")){
+            Toast.makeText(this,"Write Down Product Description",Toast.LENGTH_SHORT).show();
+        }
+        else{
+
+            HashMap<String,Object> productmap = new HashMap<>();
+            productmap.put("pid",productID);
+            productmap.put("description",pDescription);
+            productmap.put("price",pPrice);
+            productmap.put("pname",pName);
+
+            productsRef.updateChildren(productmap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+
+                    if (task.isSuccessful()){
+                        Toast.makeText(AdminMaintainProductsActivity.this,"Changes Applied Successfully", Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(AdminMaintainProductsActivity.this,AdminCategoryActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+
+                }
+            });
+
+        }
+    }
+
+
+    private void displaySpecificProductInfo() {
+        productsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if(snapshot.exists()){
+                    String pNname = snapshot.child("pname").getValue().toString();
+                    String pPrice = snapshot.child("price").getValue().toString();
+                    String pDescription = snapshot.child("description").getValue().toString();
+                    String pImage = snapshot.child("image").getValue().toString();
+
+                    name.setText(pNname);
+                    price.setText(pPrice);
+                    description.setText(pDescription);
+                    Picasso.get().load(pImage).into(imageView);
+
+
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
